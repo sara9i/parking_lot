057cc962fb2de73f933ae4bb3874e9f5e7be34ab
Sara Iftikhar <sara.iftikharsi@gmail.com>
2021-04-05 13:16:24 +0500

    clean-start in package.json ad use npm clean-start in start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ if [ ! -z "$PARKING_LOT_SIZE" ]
 then
     echo $PARKING_LOT_SIZE
     # make a temp dirrectory and resolve ts to js in it and then run via this converted js file
-    exec mkdir -p lib/src && cp -r ./src/ ./lib/src/ && tsc && node lib/index.js
+    exec npm run clean-start
 else
     echo "Did not start due to missing PARKING_LOT_SIZE"
 fi
